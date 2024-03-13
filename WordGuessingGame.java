cf4b01bd2e851aeb1e7dd1633aeec982337cabb3
Daniel Varela <202300212@estudantes.ips.pt>
2024-03-13 09:54:04 +0000

    Nível 2
    Nível 2

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -1,33 +1,75 @@
-public class WordGuessingGame
-{
-
+public class WordGuessingGame {
+    // Attributes
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
+    private InputReader reader;
 
-    public WordGuessingGame()
-    {
+    // Constructors
+    public WordGuessingGame() {
         hiddenWord = "abc";
         guessedWord = "___";
         numberOfTries = 0;
+
+        reader = new InputReader();
     }
 
-    public String getHiddenWord()
-    {
+    // Methods
+    public void play() {
+        showWelcome();
+
+        while (!hiddenWord.equals(guessedWord)) {
+            showGuessedWord();
+            System.out.println();
+            char letter = reader.getChar("Enter a letter: ");
+            guess(letter);
+        }
+
+        showResult();
+    }
+
+    private void showWelcome() {
+        System.out.println("Welcome to the Word Guessing Game!");
+    }
+
+    private void guess(char letter) {
+        boolean correctGuess = false;
+
+        for (int i = 0; i < hiddenWord.length(); i++) {
+            if (hiddenWord.charAt(i) == letter) {
+                guessedWord = guessedWord.substring(0, i) + letter + guessedWord.substring(i + 1);
+                correctGuess = true;
+            }
+        }
+
+        if (correctGuess) {
+            System.out.println("Correct! " + guessedWord);
+        } else {
+            System.out.println("Incorrect!");
+        }
+        numberOfTries++;
+    }
+
+    private void showResult() {
+        System.out.println("Congratulations! You guessed the word!");
+        System.out.println("The word is: " + hiddenWord);
+        System.out.println("Tries: " + getNumberOfTries());
+    }
+
+    // Selectors
+    public String getHiddenWord() {
         return hiddenWord;
     }
 
-    public String getGuessedWord()
-    {
+    public String getGuessedWord() {
         return guessedWord;
     }
 
-    public int getNumberOfTries()
-    {
+    public int getNumberOfTries() {
         return numberOfTries;
     }
-    
-    public void showGuessedWord(){
+
+    private void showGuessedWord() {
         System.out.print(guessedWord);
     }
 }
